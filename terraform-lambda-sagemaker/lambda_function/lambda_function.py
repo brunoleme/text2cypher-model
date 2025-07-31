6409a6117291d1170f11d185d1731b5b08e767c7
Bruno Leme <brunoleme_bl@hotmail.com>
2025-07-31 18:21:49 -0300

    changing the deployment to get the registred model.

--- terraform-lambda-sagemaker/lambda_function/lambda_function.py
+++ terraform-lambda-sagemaker/lambda_function/lambda_function.py
@@ -2,27 +2,19 @@ import boto3
 
 def lambda_handler(event, context):
     sagemaker_client = boto3.client('sagemaker')
-    model_name = event['model_name']
-    image_uri = event['image_uri']
-    model_data = event['model_data']
-    role = event['role']
+
+    model_package_arn = event['model_package_arn']
     endpoint_name = event['endpoint_name']
     instance_type = event['instance_type']
+    role = event['role']
 
-    sagemaker_client.create_model(
-        ModelName=model_name,
-        PrimaryContainer={
-            'Image': image_uri,
-            'ModelDataUrl': model_data,
-        },
-        ExecutionRoleArn=role
-    )
+    endpoint_config_name = endpoint_name + "-config"
 
     sagemaker_client.create_endpoint_config(
-        EndpointConfigName=model_name,
+        EndpointConfigName=endpoint_config_name,
         ProductionVariants=[{
             'VariantName': 'AllTraffic',
-            'ModelName': model_name,
+            'ModelPackageName': model_package_arn,
             'InitialInstanceCount': 1,
             'InstanceType': instance_type,
         }]
@@ -30,7 +22,7 @@ def lambda_handler(event, context):
 
     sagemaker_client.create_endpoint(
         EndpointName=endpoint_name,
-        EndpointConfigName=model_name
+        EndpointConfigName=endpoint_config_name
     )
 
     return {'status': 'Deployment triggered', 'endpoint_name': endpoint_name}
